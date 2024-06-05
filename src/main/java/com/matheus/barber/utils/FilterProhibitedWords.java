be0722afbe5b7175ba24e2a058bfdbc9d5b2fa8c
Matheus <cristo.matheus@outlook.com>
2024-06-05 08:01:13 -0300

    refact: Improved text filter

--- src/main/java/com/matheus/barber/utils/FilterProhibitedWords.java
+++ src/main/java/com/matheus/barber/utils/FilterProhibitedWords.java
@@ -2,6 +2,8 @@ package com.matheus.barber.utils;
 
 import com.matheus.barber.enums.ProhibitedWordsEnum;
 import com.matheus.barber.infra.exceptions.TextHasProhibitedWordsException;
+import org.ahocorasick.trie.Emit;
+import org.ahocorasick.trie.Trie;
 
 import java.util.EnumSet;
 import java.util.Set;
@@ -10,12 +12,30 @@ import java.util.stream.Collectors;
 public class FilterProhibitedWords {
 
     public static void filter(String text) {
-        String[] words = text.split(" ");
         Set<String> prohibitedWordsSet = EnumSet.allOf(ProhibitedWordsEnum.class).stream()
                 .map(ProhibitedWordsEnum::getWord)
                 .collect(Collectors.toSet());
-        for (String word : words) {
-            if (prohibitedWordsSet.contains(word.toLowerCase())) throw new TextHasProhibitedWordsException();
+        Trie.TrieBuilder builder = Trie.builder().ignoreCase().ignoreOverlaps().stopOnHit();
+        for (String prohibitedWord : prohibitedWordsSet) {
+            builder.addKeyword(prohibitedWord);
+        }
+        Trie trie = builder.build();
+        text = text.replaceAll("(?i)(\\p{L})\\d+(\\p{L})", "$1$2");
+        text = text.replaceAll("(?i)\\d+(\\p{L})", "$1");
+        text = text.replaceAll("(?i)(\\p{L})\\d+", "$1");
+        text = text.replaceAll("[^\\p{L}\\d\\s]", "");
+        Iterable<Emit> emits = trie.parseText(text);
+        for (Emit emit : emits) {
+            throw new TextHasProhibitedWordsException();
         }
     }
 }
+
+
+
+
+
+
+
+
+
